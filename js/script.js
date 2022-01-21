758e3ad4107ef900210261957da24c1561eb2825
FelipeSchmitt <50593950+FelipeSchmitt@users.noreply.github.com>
2022-01-21 13:38:33 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function request_presentation(){
+    const form = document.getElementById("form").style
+    if(form.visibility=="hidden"){
+        form.visibility="visible"
+    }
+    else{
+        form.visibility="hidden"
+    }
+}
